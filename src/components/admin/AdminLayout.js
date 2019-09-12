9a1865b5ecc65a80f130315ecfe02770d2c4c555
JunKyus <kuk941025@gmail.com>
2019-09-13 00:42:58 +0900

    Set theme

--- src/components/admin/AdminLayout.js
+++ src/components/admin/AdminLayout.js
@@ -2,14 +2,15 @@ import React from 'react'
 import makeStyles from '@material-ui/core/styles/makeStyles'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
-
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 const AdminLayout = () => {
     return (
         <Container maxWidth="md">
-            <Typography variant="body1">
+            <Typography variant="body1" >
                 hello
             </Typography>
+            <CircularProgress />
         </Container>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createMuiTheme, responsiveFontSizes, } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
+import Blue from '@material-ui/core/colors/blue'
+import LightBlue from '@material-ui/core/colors/lightBlue'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+let theme = createMuiTheme({
+    palette: {
+        primary: Blue,
+        secondary: LightBlue
+    }
+});
+theme = responsiveFontSizes(theme);
+
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
+
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
