2d04e0627f2e4aba909721dfffe6e1d6f69ab69d
JunKyus <kuk941025@gmail.com>
2019-09-15 21:35:32 +0900

    Add auto complete

--- src/components/util/AnswerPicker.js
+++ src/components/util/AnswerPicker.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import makeStyles from '@material-ui/core/styles/makeStyles'
 import PropTypes from 'prop-types'
 import TextField from '@material-ui/core/TextField'
@@ -7,31 +7,176 @@ import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Radio from '@material-ui/core/Radio'
 import Grey from '@material-ui/core/colors/grey'
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import Downshift from 'downshift'
+import deburr from 'lodash/deburr'
 
 const useStyles = makeStyles(theme => ({
     textField: {
-        margin: `${theme.spacing(1)}px 0px`
+        marginTop: `${theme.spacing(1)}px`
     },
     formControl: {
         padding: theme.spacing(1),
         border: `1px solid ${Grey['200']}`,
         overflowY: 'auto',
-        height: `calc(100% - 128px)`
-    }
+        height: `calc(100% - 128px)`,
+        marginTop: theme.spacing(1),
+    },
+    root: {
+        flexGrow: 1,
+        height: 250,
+    },
+    container: {
+        flexGrow: 1,
+        position: 'relative',
+    },
+    paper: {
+        position: 'absolute',
+        zIndex: 1,
+        marginTop: theme.spacing(1),
+        left: 0,
+        right: 0,
+    },
+    chip: {
+        margin: theme.spacing(0.5, 0.25),
+    },
+    inputRoot: {
+        flexWrap: 'wrap',
+    },
+    inputInput: {
+        width: 'auto',
+        flexGrow: 1,
+    },
+    divider: {
+        height: theme.spacing(2),
+    },
 }))
 
 const AnswerPicker = ({ selections }) => {
     const classes = useStyles();
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+        let list = [];
+        selections.forEach(selection => {
+            list.push({ label: selection.title });
+            list.push({ label: selection.subtitle })
+
+        })
+
+        list.sort((a, b) => (a.label > b.label ? 1 : a.label < b.label ? -1 : 0));
+        setSuggestions(list);
+
+    }, [selections]);
+
+    const renderInput = inputProps => {
+        const { InputProps, classes, ref, ...other } = inputProps;
+
+        return (
+            <TextField
+                className={classes.textField}
+                InputProps={{
+                    inputRef: ref,
+                    classes: {
+                        root: classes.inputRoot,
+                        input: classes.inputInput,
+                    },
+                    ...InputProps,
+                }}
+                {...other}
+            />
+        );
+    }
+
+    const renderSuggestion = suggestionProps => {
+        const { suggestion, index, itemProps, highlightedIndex, selectedItem } = suggestionProps;
+        const isHighlighted = highlightedIndex === index;
+        const isSelected = (selectedItem || '').indexOf(suggestion.label) > -1;
+
+        return (
+            <MenuItem
+                {...itemProps}
+                key={suggestion.label}
+                selected={isHighlighted}
+                component="div"
+                style={{
+                    fontWeight: isSelected ? 500 : 400,
+                }}
+            >
+                {suggestion.label}
+            </MenuItem>
+        );
+    }
+
+    const getSuggestions = (value, { showEmpty = false } = {}) => {
+        const inputValue = deburr(value.trim()).toLowerCase();
+        const inputLength = inputValue.length;
+        let count = 0;
+
+        return inputLength === 0 && !showEmpty
+            ? []
+            : suggestions.filter(suggestion => {
+                const keep =
+                    count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
+
+                if (keep) {
+                    count += 1;
+                }
+
+                return keep;
+            });
+    }
 
     return (
         <React.Fragment>
-            <TextField
-                fullWidth
-                label="정답 입력"
-                variant="outlined"
-                placeholder="이름을 입력해주세요.(성없이도 가능)"
-                className={classes.textField}
-            />
+            <Downshift>
+                {({
+                    getInputProps,
+                    getItemProps,
+                    getLabelProps,
+                    getMenuProps,
+                    highlightedIndex,
+                    inputValue,
+                    isOpen,
+                    selectedItem,
+                }) => {
+                    const { onBlur, onFocus, ...inputProps } = getInputProps({
+                        placeholder: '이름을 입력해주세요. (성없이도 가능)',
+                    });
+
+                    return (
+                        <div className={classes.container}>
+                            {renderInput({
+                                fullWidth: true,
+                                classes,
+                                label: '정답입력',
+                                InputLabelProps: getLabelProps({ shrink: true }),
+                                InputProps: { onBlur, onFocus },
+                                inputProps,
+                            })}
+
+                            <div {...getMenuProps()}>
+                                {isOpen ? (
+                                    <Paper className={classes.paper} square>
+                                        {getSuggestions(inputValue).map((suggestion, index) =>
+                                            renderSuggestion({
+                                                suggestion,
+                                                index,
+                                                itemProps: getItemProps({ item: suggestion.label }),
+                                                highlightedIndex,
+                                                selectedItem,
+                                            }),
+                                        )}
+                                    </Paper>
+                                ) : null}
+                            </div>
+                        </div>
+                    );
+
+                }}
+            </Downshift>
+
 
             <div className={classes.formControl}>
                 <FormControl component="fieldset" >
